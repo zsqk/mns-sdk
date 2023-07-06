d06746ff9f1abf15944ad4d828b9894e84e08530
iugo <iugogogo@gmail.com>
2023-07-06 17:55:35 +0800

    test: receiveMessage-fail-no-queue

--- src/queues/receive-message.test.ts
+++ src/queues/receive-message.test.ts
@@ -1,15 +1,16 @@
-import { assertEquals } from 'https://deno.land/std@0.192.0/testing/asserts.ts';
-import { receiveMessage } from "./receive-message.ts";
-import "https://deno.land/std@0.193.0/dotenv/load.ts";
+import {
+  assertIsError,
+} from 'https://deno.land/std@0.192.0/testing/asserts.ts';
+import { receiveMessage } from './receive-message.ts';
+import 'https://deno.land/std@0.193.0/dotenv/load.ts';
 
 const accountId = Deno.env.get('accountId')!;
 const regionId = Deno.env.get('regionId')!;
 const accessKeyId = Deno.env.get('accessKeyId')!;
 const accessKeySecret = Deno.env.get('accessKeySecret')!;
+const queueName = Deno.env.get('queueName')!;
 
-Deno.test('receiveMessage', async () => {
-  const queueName = 'test';
-  console.log('accountId', accountId);
+Deno.test('receiveMessage-s', async () => {
   const res = await receiveMessage({}, {
     accessKeyId,
     accessKeySecret,
@@ -18,4 +19,15 @@ Deno.test('receiveMessage', async () => {
     queueName,
   });
   console.log('res', res);
+});
+
+Deno.test('receiveMessage-fail-no-queue', async () => {
+  const res = await receiveMessage({}, {
+    accessKeyId,
+    accessKeySecret,
+    regionId,
+    accountId,
+    queueName,
+  }).catch((err) => err);
+  assertIsError(res, Error, 'queue name you provided is not exist');
 });
